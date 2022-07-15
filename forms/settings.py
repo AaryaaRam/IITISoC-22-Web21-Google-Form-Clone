b2f100e39cfcca16eefea226d91f73561ce44d03
RamPreetham9 <rampreethamt@gmail.com>
2022-07-15 16:04:14 +0530

    added signup,signin and signout features custom user page not updated

--- forms/settings.py
+++ forms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'sign.apps.SignConfig',
     'myforms.apps.MyformsConfig',
     'django.contrib.admin',
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
